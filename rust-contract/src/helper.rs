fb8b0a57786054bb00d366753a15616aafde0d1b
Steve Kok <kokchoquan@gmail.com>
2024-06-12 20:24:14 +0800

    feat: reffinance-proxy contract v0.1.0

---
+++ rust-contract/src/helper.rs
@@ -0,0 +1,14 @@
+use near_sdk::{env, AccountId, NearToken};
+
+pub fn assert_owner(owner_id: &AccountId) {
+    assert_eq!(env::predecessor_account_id(), owner_id.clone(), "Require owner.");
+}
+
+pub fn assert_full_access() {
+    assert!(env::attached_deposit() != NearToken::from_yoctonear(1), "Requires attached deposit of exactly 1 yoctoNEAR.");
+}
+
+pub fn assert_owner_and_full_access(owner_id: &AccountId) {
+    assert_owner(owner_id);
+    assert_full_access();
+}
